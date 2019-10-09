a47a04e4f2cb76ac27f9e045775d8d5ef36f17fa
syakasamate <samate015@gmail.com>
2019-10-09 07:11:49 +0000

    premier

---
+++ entities/Patient.classe.php
@@ -0,0 +1,84 @@
+<?php
+
+class PatientC{
+    private $nomP;
+    private $prenomP;
+	private $ageP;
+	private $genreP;
+    private $adresseP;
+    private $telP;
+    private $emailP;
+    public function __construct(){
+
+    }
+     function construct($nomP,$prenomP,$ageP,$genreP,$adresseP,$telP,$emailP){
+		$this->nomP=$nomP;
+        $this->prenomP=$prenomP;
+        $this->ageP=$ageP;
+		$this->genreP=$genreP;
+		$this->adresseP=$adresseP;
+		$this->telP=$telP;
+		$this->emailP=$emailP;
+		
+
+    }
+   
+	public function getNomP() {
+		return $this->nomP;
+    }
+    
+	public function setNomP($nomP) {
+		$this->nomP = $nomP;
+	}
+
+	public function getPrenomP() {
+		return $this->prenomP;
+	}
+
+	public  function setPrenomP($prenomP) {
+		$this->prenomP = $prenomP;
+	}
+
+	public  function getAgeP() {
+		return $this->ageP;
+	}
+
+	public function setAgeP($ageP) {
+		$this->ageP=$ageP;
+	}
+
+	public function getGenreP() {
+		return $this->genreP;
+	}
+
+	public function setGenreP($genreP) {
+		$this->genreP = $genreP;
+	}
+	 public function getAdresseP(){
+		 return $this->adresseP;
+	 }
+	 public function setAdresseP($adresseP){
+		 $this->adresseP=$adresseP;
+	 }
+
+	public function  getTelP() {
+		return $this->telP;
+	}
+
+	public function setTelP($telP) {
+		$this->telP = $telP;
+	}
+	public function getEmailP(){
+		return $this->emailP;
+	}
+	public function setEmailP($emailP){
+		$this->emailP=$emailP;
+	}
+   
+
+    
+    
+
+}
+
+?>
