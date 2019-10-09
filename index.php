36ad21778254b8afe93766fe3db7f21e36da0340
syakasamate <samate015@gmail.com>
2019-10-09 08:00:40 +0000

    premier

--- index.php
+++ index.php
@@ -2,13 +2,12 @@
 ini_set("display_errors",1);
 error_reporting(E_ALL);
 require_once'menu.php';
-require_once'config/DataBaseConfig.classe.php';
+require_once'config/DataBaseConfig.class.php';
 require_once'config/path.php';
-require_once'config/routing.php';
-require_once'libs/Boostrap.classe.php';
-require_once'libs/Controller.classe.php';
-require_once'libs/Model.classe.php';
-require_once'libs/View.classe.php';
+require_once'libs/Boostrap.class.php';
+require_once'libs/Controller.class.php';
+require_once'libs/Model.class.php';
+require_once'libs/View.class.php';
  $x=1;
  $boos =NULL;
  if($x>0){
