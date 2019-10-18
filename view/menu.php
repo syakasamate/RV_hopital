f7dd78df5e493ac950a177f49dc564673d43d99c
syakasamate <samate015@gmail.com>
2019-10-18 05:46:12 +0000

    modifier

--- view/menu.php
+++ view/menu.php
@@ -4,10 +4,10 @@ require_once'view/head.php';
 
  ?>
 	<body class="card text-white bg-success ">
-		<div class="navbar navbar-expand-lg navbar-light bg-light   col-md-offset-3">
-			<ul class="nav navbar-nav ">
+		<div class="navbar navbar-default navbar-fixed-top   col-md-offset-3">
+			<ul class="nav navbar-nav navbar-toggle collapsed ">
 				<li><a href="<?php echo URL.'Patient/addP';?>" >Ajout Patient </a></li>
-				<li><a href="<?php echo URL.'Patient/listeP';?>" >Liste Patient</a></li>
+				<li><a href="<?php echo URL.'Patient/listeP';?>" >Liste Patients</a></li>
 				<li><a href="<?php echo URL.'RendezVous/addR';?>">Ajout Rendez_vous</a></li>
 
             </ul>
