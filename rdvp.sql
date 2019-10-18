9ffc4e1a9becdfd20b57ef42bb900f011d0c9ed3
syakasamate <samate015@gmail.com>
2019-10-18 00:19:31 +0000

    modifier

--- rdvp.sql
+++ rdvp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:3306
--- Généré le :  Mer 09 Octobre 2019 à 10:19
+-- Généré le :  Ven 18 Octobre 2019 à 00:18
 -- Version du serveur :  5.7.26-0ubuntu0.18.04.1
 -- Version de PHP :  7.2.19-0ubuntu0.18.04.1
 
@@ -31,6 +31,14 @@ CREATE TABLE `Domaine` (
   `Nom_Dom_Domaine` varchar(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Contenu de la table `Domaine`
+--
+
+INSERT INTO `Domaine` (`Id_Dom_Domaine`, `Nom_Dom_Domaine`) VALUES
+(5, 'cardiologue'),
+(6, 'chirurgien');
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +54,14 @@ CREATE TABLE `Medcin` (
   `Id_Serv_Service` int(40) DEFAULT NULL,
   `Id_Dom_Domaine` int(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `Medcin`
+--
+
+INSERT INTO `Medcin` (`Id_Med_Medcin`, `Nom_Med_Medcin`, `Prenom_Med_Medcin`, `Tel_Med_Medcin`, `Email_Med_Medcin`, `Id_Serv_Service`, `Id_Dom_Domaine`) VALUES
+(10, 'gueye', 'saliou', '779451231', 'gueya@gmail.com', 4, 5),
+(11, 'seydi', 'aby', '769801254', 'aby@gmail.com', 5, 6);
 
 -- --------------------------------------------------------
 
@@ -69,7 +85,8 @@ CREATE TABLE `Patient` (
 --
 
 INSERT INTO `Patient` (`id_P_Patient`, `Nom_p`, `Prenom_p`, `Age_p`, `Genre_P`, `Tel_p`, `Adresse_p`, `Email_p`) VALUES
-(2, 'cisse', 'falou', '21 ans', 'masculin', '774569887', 'Dakar', 'falou@gmail.com');
+(4, 'cisse', 'falou', '21 ans', 'M', '772304521', 'Dakar', 'falou@gmail.com'),
+(5, 'samb', 'sire', '19 ans', 'F', '789201336', 'pikine', 'samb@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -79,11 +96,19 @@ INSERT INTO `Patient` (`id_P_Patient`, `Nom_p`, `Prenom_p`, `Age_p`, `Genre_P`, 
 
 CREATE TABLE `Planning` (
   `Id_Pl_Planning` int(40) NOT NULL,
-  `Date_Pl_Planning` varchar(40) DEFAULT NULL,
+  `Date_Pl_Planning` date DEFAULT NULL,
   `Heure_Deb_Planning` time DEFAULT NULL,
   `Heure_Fin_Planning` time DEFAULT NULL,
   `Id_Med_Medcin` int(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `Planning`
+--
+
+INSERT INTO `Planning` (`Id_Pl_Planning`, `Date_Pl_Planning`, `Heure_Deb_Planning`, `Heure_Fin_Planning`, `Id_Med_Medcin`) VALUES
+(3, '2019-10-01', '08:00:00', '15:00:00', 10),
+(4, '2019-10-01', '08:05:00', '14:00:00', 11);
 
 -- --------------------------------------------------------
 
@@ -93,11 +118,19 @@ CREATE TABLE `Planning` (
 
 CREATE TABLE `Rv` (
   `Id_rv_rv` int(40) NOT NULL,
-  `Heure_rv_rv` time DEFAULT NULL,
-  `Date_rv_rv` date DEFAULT NULL,
+  `Heure_rv` time DEFAULT NULL,
+  `Date_rv` date DEFAULT NULL,
   `Id_Med_Medcin` int(40) DEFAULT NULL,
   `id_P_Patient` int(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `Rv`
+--
+
+INSERT INTO `Rv` (`Id_rv_rv`, `Heure_rv`, `Date_rv`, `Id_Med_Medcin`, `id_P_Patient`) VALUES
+(2, '08:00:00', '2019-10-01', 10, 4),
+(3, '08:45:00', '2019-10-01', 10, 5);
 
 -- --------------------------------------------------------
 
@@ -114,6 +147,14 @@ CREATE TABLE `secretaire` (
   `Id_Serv_Service` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Contenu de la table `secretaire`
+--
+
+INSERT INTO `secretaire` (`Id_Secret_sercretaire`, `Nom_Secret_sercretaire`, `Prenom_Secret_sercretaire`, `Tel_Secret_sercretaire`, `Email_Secret_sercretaire`, `Id_Serv_Service`) VALUES
+(5, 'ndour', 'maty', '702301514', 'maty@gmail.com', 4),
+(6, 'sow', 'amadou', '789502123', 'sow@gmail.com', 5);
+
 -- --------------------------------------------------------
 
 --
@@ -125,6 +166,14 @@ CREATE TABLE `Service` (
   `Nom_Serv_Service` varchar(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Contenu de la table `Service`
+--
+
+INSERT INTO `Service` (`Id_Serv_Service`, `Nom_Serv_Service`) VALUES
+(4, 'insuffisance_cardiaque'),
+(5, 'transplantation');
+
 -- --------------------------------------------------------
 
 --
@@ -134,15 +183,18 @@ CREATE TABLE `Service` (
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `login` varchar(45) NOT NULL,
-  `pasword` varchar(45) NOT NULL
+  `pasword` varchar(45) NOT NULL,
+  `profil` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `user`
 --
 
-INSERT INTO `user` (`id`, `login`, `pasword`) VALUES
-(1, 'samate015@gmail.com', 'samate');
+INSERT INTO `user` (`id`, `login`, `pasword`, `profil`) VALUES
+(1, 'secret015@gmail.com', '123', 'secretaire'),
+(2, 'admin@gmail.com', 'admin', 'admin'),
+(3, 'medcin@gmail.com', '123', 'medcin');
 
 --
 -- Index pour les tables exportées
@@ -210,42 +262,42 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `Domaine`
 --
 ALTER TABLE `Domaine`
-  MODIFY `Id_Dom_Domaine` int(40) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_Dom_Domaine` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT pour la table `Medcin`
 --
 ALTER TABLE `Medcin`
-  MODIFY `Id_Med_Medcin` int(40) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_Med_Medcin` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT pour la table `Patient`
 --
 ALTER TABLE `Patient`
-  MODIFY `id_P_Patient` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_P_Patient` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT pour la table `Planning`
 --
 ALTER TABLE `Planning`
-  MODIFY `Id_Pl_Planning` int(40) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_Pl_Planning` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT pour la table `Rv`
 --
 ALTER TABLE `Rv`
-  MODIFY `Id_rv_rv` int(40) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_rv_rv` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT pour la table `secretaire`
 --
 ALTER TABLE `secretaire`
-  MODIFY `Id_Secret_sercretaire` int(40) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_Secret_sercretaire` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT pour la table `Service`
 --
 ALTER TABLE `Service`
-  MODIFY `Id_Serv_Service` int(40) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_Serv_Service` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- Contraintes pour les tables exportées
 --
