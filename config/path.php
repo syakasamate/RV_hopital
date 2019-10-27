0b8d128782a487015df4372919cfb5893b67665d
syakasamate <samate015@gmail.com>
2019-10-27 02:01:45 +0000

    modifier

--- config/path.php
+++ config/path.php
@@ -1,5 +1,11 @@
 <?php
 //la route de notre projet
 define('URL','http://localhost/RV_hopital/');
+define('LISTEP','patient/liste.php');
+define('LISTEM','medcin/liste.php');
+define('LISTESEC','secretaire/liste.php');
+define('LISTES','service/liste.php');
+define('LISTEPL','planing/liste.php');
+define('LISTERV','rendez_vous/liste.php');
 
 ?>
