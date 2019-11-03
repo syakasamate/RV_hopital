741c6c2c91037a45b3f1ecdc8d239298f5dcd28b
syakasamate <samate015@gmail.com>
2019-11-03 07:20:42 +0000

    modifier

--- view/rendez_vous/add.php
+++ view/rendez_vous/add.php
@@ -1,29 +1,18 @@
-            <?php require_once'view/head.php';
+            <?php
+           ini_set("display_errors",1);
+           error_reporting(E_ALL);
+             require_once'view/head.php';
             require_once'view/headS.php';
             $donne=$donne+1;
         $r=sprintf("%05d",$donne);
         ?>
-            ?>
+            
 
             <body class="card text-white bg-info" >
             <div class="container col-md-6 col-xs-10 col-md-offset-3" style="margin-top:-500px;">
                 <div class="panel panel-info">
                 <div class="panel-heading">Ajout Rendez_Vous </div>
                 <div class="panel-body">
-                <?php
-					if(!empty($donne1)){
-						if ($donne1 != 0){
-                            ?>
-                            <div class="alert alert-success">Données ajoutées!</div>
-                            <?php
-						}else{
-                            ?>
-                            <div class="alert alert-danger">Erreur!</div>
-                            <?php
-                        }
-                        }
-       ?>
-              
             <form action="<?php echo URL.'RendezVous/addR';?>"   method="post">
                 <div class="form-group">
                 <label for="" class="form control-label">Code Rv</label>
@@ -33,6 +22,20 @@
                 <label for="" class="form control-label">Date Et Heure</label>
                 <input type="datetime-local" class="form-control" name="dateRv">
                 </div>
+                <?php 
+               if(!empty($donne1)){
+                 ?>
+                <div class="alert alert-danger"><?=$donne1?></div>
+                <?php
+               }
+               ?>
+               <?php 
+               if(!empty($donne2)){
+                 ?>
+                <div class="alert alert-danger"><?=$donne2?></div>
+                <?php
+               }
+               ?>
                 <div class="form-group">
                 <label for="" class="form control-label">Nom Medecin</label>
                 <select    class="form-control" name="nomM" id="nomM">
@@ -56,6 +59,7 @@
                 <?php 
                 foreach($dat as $pat ){
                 foreach($pat as $patient){
+          
                     ?>
                 <option value="<?= $patient[0]?>"><?=$patient[2]?></option>
                 <?php
